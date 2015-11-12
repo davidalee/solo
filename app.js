2e9b4e6fe9f563e4c0e0393823e307e93188f29e
David Lee <david7lee@gmail.com>
2015-11-11 22:03:54 -0500

    sample data in place, successfully making call to watson api

--- app.js
+++ app.js
@@ -1,13 +1,102 @@
 var express = require('express');
-
+var path = require('path');
+var fs = require('fs');
+var watson = require('./scripts/watson');
 var app = express();
 
 // app.set('views', __dirname + '/views');
 // app.set('view engine', 'ejs');
-app.use(express.static(__dirname + '/public'));
+app.use(express.static(__dirname));
 
-// app.get('/', function(req, res){
-//   res.render('index');
+// **** REMOVE BELOW *****
+// SAMPLE DATA ***** REMOVE once music lyric API is hooked up
+var kanyePath = path.join(__dirname, '/text_samples/kanye_2015.txt');
+var someText;
+
+fs.readFile(kanyePath, 'utf8', function(err, data){
+  if( err ){
+    console.log(err);
+  } else { 
+    someText = data;
+  }
+});
+// **** REMOVE ABOVE *****
+
+app.get('/search', function(req, res){
+  // This is our search query
+  var query = req._parsedUrl.query; 
+
+  // TODO: Send search query to music lyric API
+
+  watson.profile({
+    text: someText,
+    language: 'en',
+    acceptLanguage: 'en-US'
+  },
+
+  // TODO: Accept URLs as search queries
+  // grab HTML of site, send to watson as content-type: HTML
+
+  function (err, profile) {
+    if( err ){
+      console.log('watson error: ', err);
+    } else {
+      res.json(profile);
+    }
+  })
+});
+
+
+// *****
+// The code snippet below is for the ajax call being used in demo implementation
+// *****
+
+// app.post('/api/profile', function(req, res){
+//   console.log('hi', req.body);
+//   // var query = req._parsedUrl.query; 
+
+//   // TODO: Send search query to music lyric API
+
+//   watson.profile({
+//     text: someText,
+//     language: 'en',
+//     acceptLanguage: 'en-US'
+//   },
+
+//   // TODO: Accept URLs as search queries
+//   // grab HTML of site, send to watson as content-type: HTML
+
+//   function (err, profile) {
+//     if( err ){
+//       console.log('watson error: ', err);
+//     } else {
+//       // var paragraphs = watson.textSummary.assemble(profile.tree);
+//       console.log('yo', paragraphs);
+//       res.json(profile);
+//     }
+//   })
+// });
+
+
+// personality_insights.profile({
+//   text: data,
+//   language: 'en' },
+//   function (err, response) {
+//     if (err)
+//       console.log('watson error:', err);
+//     else
+//       console.log(JSON.stringify(response, null, 2));
 // });
 
 app.listen(3000);
+
+// app.get('/api/profile', function(req, res, next) {
+//   var parameters = extend(req.body, { acceptLanguage : i18n.lng() });
+
+//   personalityInsights.profile(parameters, function(err, profile) {
+//     if (err)
+//       return next(err);
+//     else
+//       return res.json(profile);
+//   });
+// });
---
+++ scripts/helper.js
@@ -0,0 +1,9 @@
+var search = function(query){
+  // Pulls data from somewhere, whether it's music lyric api or twitter api or whatever
+  console.log('search helper func called');
+  queryWatson(query);
+};
+
+var queryWatson = function(query){
+  console.log('querying watson');
+};
